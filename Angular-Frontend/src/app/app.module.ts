07c1f22d7d8077e42958ad8af8e99bdbdaad4fd8
sbmano <sbmano2000@gmail.com>
2022-12-01 23:47:51 +0530

    Full Stack App

---
+++ Angular-Frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeService } from './employee.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular-Frontend/src/app/employee.ts
@@ -0,0 +1,9 @@
+export class Employee {
+    id!: number;
+    firstName!: string;
+    lastName!: string;
+    emailId!: string;
+}
+
+/*The non-null assertion operator(!) removes 'null' and 
+'undefined' for a type without doing any explicit type checking.*/
